de2d6893d8a011439e9a79694b137fc2f09836f9
AlexProkopev <129211387+AlexProkopev@users.noreply.github.com>
2025-07-30 18:39:00 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-
-import ReactDOM from "react-dom/client";
 import "./index.css";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
